ce0335316a1fb47edb75e3c2aed62fc5297c27fb
adupoku71 <davidpoku71@gmail.com>
2026-06-13 13:27:10 +0000

    initial version of project: everything cramped in the main class

--- src/main/java/com/david/musicplayer/Main.java
+++ src/main/java/com/david/musicplayer/Main.java
@@ -1,8 +1,59 @@
 package com.david.musicplayer;
 
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Initial setup for music player project");
+
+        String filePath = "src/main/resources/audio/on-the-ground-by-kevin-macleod.wav";
+        File file = new File(filePath);
+
+        try(Scanner scanner = new Scanner(System.in);AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)) {
+
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+            String response = "";
+
+            while(!response.equals("Q")) {
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter your choice: ");
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Could not locate file");
+        }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file is not supported");
+        }
+        catch (LineUnavailableException e) {
+            System.out.println("Unable to access audio");
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong");
+        }
+        finally {
+            System.out.println("Bye");
+        }
     }
 }
