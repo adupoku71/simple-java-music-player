1e72863f50f8525810c4f94b4fb705a9004420d3
adupoku71 <davidpoku71@gmail.com>
2026-06-13 11:59:16 +0000

    Simple music player setup

---
+++ src/main/java/com/david/musicplayer/Main.java
@@ -0,0 +1,8 @@
+package com.david.musicplayer;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initial setup for music player project");
+    }
+}
